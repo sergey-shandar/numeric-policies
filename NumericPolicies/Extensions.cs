7af29eaedd5ef1c6e1d9f1dfca8f26184a20a88f
Sergey Shandar <sergey@live.com.au>
2017-07-03 22:58:23 -0700

    MomentList1

---
+++ NumericPolicies/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace NumericPolicies
+{
+    public static class NumericExtensions
+    {
+        public static MomentList1<T, P> ToMomentList1<T, P>(this P _, T value, long count)
+            where P : struct, INumericPolicy<T>
+            => new MomentList1<T, P>(value, count);
+
+        public static MomentList1<T, P> ToMomentList1<T, P>(this P _, T value)
+            where P : struct, INumericPolicy<T>
+            => new P().ToMomentList1(value, 1);
+
+        public static MomentList1<T, P> ToMomentList1<T, P>(this P _)
+            where P : struct, INumericPolicy<T>
+            => new P().ToMomentList1(new P().FromLong(0), 0);
+    }
+}
---
+++ NumericPolicies/MomentList1.cs
@@ -0,0 +1,22 @@
+﻿namespace NumericPolicies
+{
+    public struct MomentList1<T, P>
+        where P : struct, INumericPolicy<T>
+    {
+        public T Sum { get; }
+
+        public long Count { get; }
+
+        public MomentList1(T sum, long count)
+        {
+            Sum = sum;
+            Count = count;
+        }
+
+        public MomentList1<T, P> Add(MomentList1<T, P> b)
+            => new P().ToMomentList1(new P().Add(Sum, b.Sum), Count + b.Count);
+
+        public T Average
+            => new P().Div(Sum, new P().FromLong(Count));
+    }
+}
